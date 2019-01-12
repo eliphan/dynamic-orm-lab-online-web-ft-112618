4c4efc30cf2ec9bbff438f4db150c38be5ca9ad3
Elise Phan <aliceconnic@gmail.com>
2019-01-12 03:26:37 +0000

    Automatically backed up by Learn

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -52,7 +52,11 @@ def save
   @id = DB[:conn].execute("SELECT last_insert_rowid() FROM #{table_name_for_insert}")[0][0]
 end  
 
-
+  def self.find_by_name(name)
+    sql = "SELECT * FROM #{self.table_name} WHERE name = '#{name}'"
+    DB[:conn].execute(sql)
+  end
+  
   def self.find_by(attribute_hash)
     value = attribute_hash.values.first
     formatted_value = value.class == Fixnum ? value : "'#{value}'"
